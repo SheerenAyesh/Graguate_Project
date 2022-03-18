4b7bb5772e2b16234471e3f996fab28dffb25e2d
ahmadawwad <ahmad111awwad@gmail.com>
2022-03-18 16:43:50 +0200

    v3

--- app/src/main/java/com/example/graduation_project/MainActivity.java
+++ app/src/main/java/com/example/graduation_project/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.graduation_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void searchPart(View view) {
+        Intent intent = new Intent(this ,searchpart.class);
+       // intent.putExtra("username",uname);
+        startActivity(intent);
+    }
+
+    public void mechanic(View view) {
+        Intent intent = new Intent(this ,mechanic.class);
+        // intent.putExtra("username",uname);
+        startActivity(intent);
+    }
+
+    public void truck(View view) {
+        Intent intent = new Intent(this ,truck.class);
+        // intent.putExtra("username",uname);
+        startActivity(intent);
+    }
+
+    public void homepage(View view) {
+        Intent intent = new Intent(this ,homePage.class);
+        // intent.putExtra("username",uname);
+        startActivity(intent);
+    }
+
+    public void setting(View view) {
+        Intent intent = new Intent(this ,setting.class);
+        // intent.putExtra("username",uname);
+        startActivity(intent);
+    }
+
+    public void updateProfile(View view) {
+        Intent intent = new Intent(this ,updateProfile.class);
+        // intent.putExtra("username",uname);
+        startActivity(intent);
     }
 }
