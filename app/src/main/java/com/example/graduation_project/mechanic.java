99adbc0949db4bb5400573cf80985c2cb4b5f7c8
ahmadawwad <ahmad111awwad@gmail.com>
2022-05-09 14:49:51 +0300

    shh

--- app/src/main/java/com/example/graduation_project/mechanic.java
+++ app/src/main/java/com/example/graduation_project/mechanic.java
@@ -143,9 +143,14 @@ private RequestQueue queue;
     public void go_to_det(int i) {
         Intent intent = new Intent(this ,mecdetails.class);
         intent.putExtra("username",arr[i]);
+        if(lat!=null)
         intent.putExtra("latitude",lat);
+        else
+            intent.putExtra("latitude","40.55");
+if(log!=null)
         intent.putExtra("longitude",log);
-
+else
+    intent.putExtra("longitude","40.55");
         startActivity(intent);
     }
 
